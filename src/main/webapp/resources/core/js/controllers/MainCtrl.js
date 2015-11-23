02a31fa633e0fe7ddf2f0efe3f88369a2bc4072e
unknown <ricardomachado.sb@gmail.com>
2015-11-23 07:13:32 -0200

    Implementing voting functionality

--- src/main/webapp/resources/core/js/controllers/MainCtrl.js
+++ src/main/webapp/resources/core/js/controllers/MainCtrl.js
@@ -1,7 +1,7 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope, $http, $rootScope) {
 	
 	$scope.formData = {};
-
+		
 	$scope.list = function(){
     	$http.get('/api/restaurant/list').success(function(res){
 			$scope.restaurants = res;
